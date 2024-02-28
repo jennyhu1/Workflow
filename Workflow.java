d986077307aa896050e297899d99eecb791c7138
Megz-Mugz <rohan.mengle123@gmail.com>
2024-02-28 18:59:39 -0500

    finished in class exercise

--- Workflow.java
+++ Workflow.java
@@ -1,7 +1,19 @@
 class Workflow{
 
-    public static void main(String[] args) {
-    
+    private int wfid;
+    private String step;
+    private int objid;
+
+    public boolean AddWFItem(String step, int objid){
+        return true;
+    }
+
+    public int GetNextWFItem(String step){
+        return objid;
+    }
+
+    public int CountWFItems(String step){
+        return 1;
     }
 
 }
